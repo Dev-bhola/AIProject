8f6b56f69e6d8d5b721c2cf2eba90368943add02
Dev <devbhola1902@gmail.com>
2025-09-05 10:13:13 +0530

    Finished all the apis

--- server/server.js
+++ server/server.js
@@ -3,8 +3,10 @@ import cors from "cors";
 import 'dotenv/config'
 import {clerkMiddleware, requireAuth} from "@clerk/express"
 import aiRouter from "./Routes/aiRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import userRouter from "./Routes/userRoutes.js";
 const app=express()
-
+await connectCloudinary();
 app.use(cors())
 app.use(express.json())
 app.use(clerkMiddleware());
@@ -13,6 +15,7 @@ app.get('/',(req,res)=>{
 })
 app.use(requireAuth())
 app.use('/api/ai',aiRouter);
+app.use("/api/user", userRouter);
 const PORT=process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`);
